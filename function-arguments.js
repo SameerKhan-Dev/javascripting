67bb930bf96d084ebb512a170bcb921d30fd04d1
Sameer Khan <sameerk_786@hotmail.com>
2020-11-13 08:34:07 +0000

    added objects and functions exercises files

---
+++ function-arguments.js
@@ -0,0 +1,9 @@
+
+// this simple function takes three arguments, does a calculation and returns output.
+function math (firstParam,secondParam,thirdParam){
+
+  return firstParam + (secondParam * thirdParam);
+}
+
+console.log(math(53,61,67));
+
